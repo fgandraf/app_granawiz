6678458f9d4a95c8ceab3668286869703233fdbe
Felipe Gandra <41455881+fgandraf@users.noreply.github.com>
2024-11-07 12:30:54 -0300

    Add object IconPaths

---
+++ app/src/main/kotlin/config/IconPaths.kt
@@ -0,0 +1,7 @@
+package config
+
+object IconPaths {
+    const val SYSTEM_ICONS = "assets/icons/systemIcons/"
+    const val BANK_LOGOS = "assets/icons/bankLogos/"
+    const val CATEGORY_PACK = "assets/icons/categoryPack/"
+}
