d8822b421000dc204ed000cbb33b01153ab770a5
Felipe Gandra <41455881+fgandraf@users.noreply.github.com>
2024-11-07 12:14:41 -0300

    feat: add Compose Desktop project w/ package structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.jpa") version "1.9.10"
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "com.felipegandra"
+version = "1.0.0-Alpha"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.components.resources)
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    // Driver JDBC para SQLite
+    implementation("org.xerial:sqlite-jdbc:3.41.2.2")
+
+    // Hibernate
+    implementation("org.hibernate.orm:hibernate-core:6.3.1.Final") // Hibernate Core
+    implementation("org.hibernate.orm:hibernate-hikaricp:6.3.1.Final") // Gerenciamento de conexões
+    implementation("org.hibernate.orm:hibernate-community-dialects:6.3.1.Final") // Community Dialects
+
+    // Flyway
+    implementation("org.flywaydb:flyway-core:9.22.0") // Flyway para migrações
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "MoneyMap"
+            packageVersion = "1.0.0"
+        }
+    }
+}
